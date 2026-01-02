63836c901db1c8ba1a258570a8c3711f048eb4d4
kmmaheshsharma <49545665+kmmaheshsharma@users.noreply.github.com>
2026-01-02 08:42:12 +0530

    Add files via upload

--- node/public/sw.js
+++ node/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nobroko-cache-v12";
+const CACHE_NAME = "nobroko-cache-v13";
 const urlsToCache = ["/", "/index.html", "/app.js", "/styles.css", "/manifest.json"];
 
 self.addEventListener("install", (e) => {
